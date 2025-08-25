6599bf0bc583bf0d177dbe93b41872f7a11492c6
Rodriguez, Milton <mnrodriguez@institutoseguro.com.ar>
2025-08-25 12:34:34 -0300

    seeder modificado

--- server/database/seeders/RolSeeder.php
+++ server/database/seeders/RolSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+use Hash;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -8,16 +9,37 @@ use Illuminate\Database\Seeder;
 
 class RolSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-            DB::table('rols')->insert([
-            'id' => 1,
-            'nombre_rol' => 'admin',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-        ]);
+        DB::table('rols')->updateOrInsert(
+            ['nombre_rol' => 'admin'],
+            ['created_at' => now(), 'updated_at' => now()]
+        );
+
+        DB::table('rols')->updateOrInsert(
+            ['nombre_rol' => 'secretario'],
+            ['created_at' => now(), 'updated_at' => now()]
+        );
+
+        DB::table('rols')->updateOrInsert(
+        ['nombre_rol' => 'empleado'],
+            ['created_at' => now(), 'updated_at' => now()]
+        );
+        DB::table('users')->updateOrInsert(
+    // Clave para evitar duplicados
+    ['email' => 'admin@example.com'],
+    // Datos a insertar/actualizar
+    [
+        'nombre'         => 'Admin',
+        'apellido'       => 'Sistema',
+        'nombre_usuario' => 'admin',
+        'password'       => Hash::make('12345678'), 
+        'id_rol'         => 1, 
+        'bloqueado'      => false,
+        'created_at'     => now(),
+        'updated_at'     => now(),
+    ]
+);
     }
 }
+
